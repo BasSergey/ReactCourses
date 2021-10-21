dea5f95e3a117465e42866bbbc5d896974228115
Sergey Bas <sstyleimage@gmail.com>
2021-10-21 21:12:27 +0300

    Table search

--- app/src/components/NavBar.jsx
+++ app/src/components/NavBar.jsx
@@ -1,18 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav>
-    <div className="nav-wrapper">
-      <a href="#" className="brand-logo">Logo</a>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="#">Sass</a></li>
-        <li><a href="#">Components</a></li>
-        <li><a href="#">JavaScript</a></li>
-      </ul>
-    </div>
-  </nav>
- 
-  ); 
+      <div className="nav-wrapper">
+        <a href="#" className="brand-logo">Logo</a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><Link to='/users'>Users</Link></li>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/users'>Users</Link></li>
+        </ul>
+      </div>
+    </nav>
+
+  );
 }
 export default NavBar;
--- app/src/components/UserList.jsx
+++ app/src/components/UserList.jsx
@@ -1,44 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const UserList = (props) =>  {
-const onChange = (e) =>{
-    console.log(e.target.value)
-}
+const UserList = (props) => {
+    const [filter, setFilter] = useState(props.children)
+
+    useEffect(() => {  //хук появляется сразу после загрузки страницы
+        setFilter(props.children) //присваивает
+    }, [props.children]) //за чем будем следить 
+
+    const getSearch = () => {
+        if (filter) { //если данный в fiter есть 
+            return filter
+        }
+        return props.children
+    }
+    const userSearch = getSearch();
+    const onChange = (e) => {
+        // console.log(e.target.value)
+        setFilter(props.children.filter((user) => user.name.toLowerCase().includes(e.target.value.toLowerCase())))
+    }//перебирание пользоватей с выводом в консоль(до сейчас setFilter ), toLowerCase() для того чтобы в независимости от высокого низкого текста, он выводился
+    //Если мы юы хотели написать все выше в нельсколько строк, то нужно было бы ((user)=>{return .... }
+
     return (
         <>
-        
-        { props.search &&  
-        
-        <div class="row">
-                <div class="input-field col s6">
-                <i class="material-icons prefix">search</i>
-                <textarea id="icon_prefix2" class="materialize-textarea"
-                 onChange={onChange}></textarea>
-                
-                </div>
-            </div> }
-        <table>
-        <thead>
-          <tr>
-              <th>Name</th>
-              <th>Phone</th>
-              <th>Delete</th>
-          </tr>
-        </thead>
 
-        <tbody>
-         { props.children && props.children.map((user)=>
-         <tr>
-            <td>{user.name}</td>
-            <td>{user.phone}</td>
-            <i className="material-icons"
-            onClick={()=>props.deleteUser(user.id)}>
-                delete</i>
+            {props.search &&
 
-          </tr>
-           )}
-        </tbody>
-      </table>
+                <div className="row">
+                    <div className="input-field col s6">
+                        <i className="material-icons prefix">search</i>
+                        <textarea id="icon_prefix2" className="materialize-textarea"
+                            onChange={onChange}
+                            placeholder="Search by name"></textarea>
+
+                    </div>
+                </div>}
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Phone</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {props.children && filter.map((user) =>
+                        <tr key={user.id} >
+                            <td>{user.name}</td>
+                            <td>{user.phone}</td>
+                            <i className="material-icons"
+                                onClick={() => props.deleteUser(user.id)}>
+                                delete</i>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
         </>
     )
 }
